e58c203455a0bb39a06c16ccf32a96443b82dfe1
anandhu336 <anandhakrishnanbabu@gmail.com>
2026-09-20 14:44:39 +0100

    update

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
    Both values are safe to publish: the "anon / publishable" key is meant to be public.
    Never paste the "service_role" or "secret" key here. */
 window.KS_CONFIG = {
-  SUPABASE_URL: '',       // e.g. https://abcdxyz.supabase.co
-  SUPABASE_ANON_KEY: ''   // the anon / publishable key
+  SUPABASE_URL: 'https://nmplyauokwlxwwejzomv.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tcGx5YXVva3dseHd3ZWp6b212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5MDc3NTAsImV4cCI6MjEwNTQ4Mzc1MH0.F1OffwdfY9YRw5lOxZteedoChVreX-PcoXouX5yxjxM'
 };
